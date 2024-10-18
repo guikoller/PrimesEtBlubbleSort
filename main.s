cb7cf5a22f5331a3ee8ac34feeca616dbf7e7921
guikoller <correa.koller@tecpuc.com.br>
2024-10-18 19:47:05 -0300

    check_prime function implemented

--- main.s
+++ main.s
@@ -8,6 +8,9 @@
 ; -------------------------------------------------------------------------------
 ; Declarações EQU - Defines
 ;<NOME>         EQU <VALOR>
+
+RANDOM_LIST		EQU		0x20000A00
+PRIME_LIST		EQU 	0x20000B00
 ; -------------------------------------------------------------------------------
 ; Área de Dados - Declarações de variáveis
 		AREA  DATA, ALIGN=2
@@ -36,7 +39,106 @@
 Start  
 ; Comece o código aqui <======================================================
 
-    ADDS R0,#1;
+    LDR R0, =RANDOM_LIST; ;ponteiro para lista original
+	LDR R1, =PRIME_LIST; ; ponteiro para lista de primos
+	MOV R2, #20;		;TAM
+	
+loadlist
+	MOV R3, #193;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #63;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #176;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #127;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #43;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #13;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #211;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #3;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #203;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #5;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #21;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #7;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #206;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #245;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #157;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #237;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #241;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #105;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #252;
+	STRB R3, [R0], #4;
+	
+	MOV R3, #19;
+	STRB R3, [R0], #4;
+	
+find_primes
+	LDR R0, =RANDOM_LIST;
+	MOV R4, #0;COUNT PRIMOS
+	MOV R9,#0;
+	
+check_prime_loop
+	LDRB R5, [R0], #4;
+	;B next_num
+	
+	
+	MOV R6, #2; ;inicia o divisor com 2
+check_prime
+	UDIV R7, R5, R6; ; R7=R5/R6
+	MLS R8, R7, R6, R5; ;resto = r5 - (r6*r7)
+	
+	CMP R8, #0;
+	BEQ next_num
+	
+	ADD R6, R6, #1;
+	CMP R6, R5;
+	IT LT
+		BLT check_prime
+		
+	;;Numero é primo: salvar na lista
+	STRB R5, [R1], #4;
+	ADD R4, R4, #1;
+	
+next_num
+	ADD R9, R9, #1; incrementa indice
+	CMP R2, R9; compara com o tamanho da lista
+	IT GT
+		BGT check_prime_loop
+	
+
+	
 	
 	NOP;
 	
